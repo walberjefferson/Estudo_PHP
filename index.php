5a5d9484c175635e327c41b56add371bc14d6a41
Walber Jefferson <walberjefferson@hotmail.com>
2015-08-17 17:44:06 -0300

    Tratando erros

--- index.php
+++ index.php
@@ -1,19 +1,60 @@
+<?php
+#error_reporting(E_ALL);
+#ini_set("display_errors", 1);
+
+require_once 'Classe/Carro.php';
+require_once 'Classe/Calculo.php';
+
+use Classe\Carro;
+use Classe\Calculo;
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
 	<title>Estudo PHP</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-	<?php
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<hr>
+				<?php
 
-	require_once 'Classe/Carro.php';
+				$carro = new Carro('Grafite', 1994, 'Voyage', 'Volksvagen');
+				echo '<p class="text-center">'.$carro->constroiCarro().'</p>';
 
-	use Classe\Carro;
+				$transporte = new ArrayIterator(array(
+					'carro', 
+					'moto', 
+					'bicicleta', 
+					'cavalo',
+					'jegue'
+				));
 
-	$carro = new Carro('Branco', 1994, 'Voyage', 'Volksvagen');
-	echo $carro->constroiCarro();
+				foreach (new LimitIterator($transporte, 1) as $k => $trans) {
+					echo $k . ' - ' . strtoupper($trans) . '<br>';
 
-	?>
+				}
+
+				?>
+
+				<hr>
+
+				<?php
+					$calculo = new Calculo;
+
+					try{
+						echo $calculo->soma(10, 'oi');
+					}catch(Exception $e){
+						echo 'Houve um erro durante o calculo: ' . $e->getMessage();
+					}
+					
+				?>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
